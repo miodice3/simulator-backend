b06f33748835b13c78abab0daa031263af7c781b
Michael Iodice <miodice3@gmail.com>
2021-01-26 15:35:00 -0700

    created index routes, strong params, fixed authentication issue in application controller

--- app/controllers/appliances_controller.rb
+++ app/controllers/appliances_controller.rb
@@ -1,14 +1,26 @@
 class AppliancesController < ApplicationController
 
-    def index
-        appliances = Appliance.all
-        render json: ApplianceSerializer.new(appliances).to_serialized_json
-      end
-    
-    def show
-        appliance = Appliance.find_by(id: params[:id])
-        # render json: appliance, except: [:created_at, :updated_at]
-        render json: ApplianceSerializer.new(appliance).to_serialized_json
-      end
+  def create
+    appliance = Appliance.create(appliance_params)
+    # byebug
+    render json: ApplianceSerializer.new(appliance).to_serialized_json
+  end
+
+  def index
+    appliances = Appliance.all
+    render json: ApplianceSerializer.new(appliances).to_serialized_json
+  end
+  
+  def show
+    appliance = Appliance.find_by(id: params[:id])
+    render json: ApplianceSerializer.new(appliance).to_serialized_json
+  end
+
+  private
+
+  def appliance_params
+    params.require(:appliance).permit(:name, :wattage)
+  end
+
 
 end
