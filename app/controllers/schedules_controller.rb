3141dfa0de9096a79f8e00e8a4d9fd47a1161cd6
Michael Iodice <miodice3@gmail.com>
2021-01-29 14:44:33 -0700

    added update for schedules from slider functionality

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -4,6 +4,23 @@ class SchedulesController < ApplicationController
         schedule = Schedule.create(schedule_params)
     end
 
+    def update
+
+        schedule = Schedule.find_by(id: update_params[:id])
+
+        if params[:left_right] == "slider-left" 
+            schedule.time_on = update_params[:time_on]+":00"
+            schedule.save
+        elsif params[:left_right] == "slider-right" 
+            schedule.time_off = update_params[:time_off]+":00"
+            schedule.save
+        end
+
+        appliances = Appliance.all
+        render json: ApplianceSerializer.new(appliances).to_serialized_json
+
+    end
+
     def destroy
         schedule = Schedule.find_by_id(params[:id])
         schedule.destroy
@@ -17,6 +34,10 @@ class SchedulesController < ApplicationController
 
     def schedule_delete_params
         params.require(:schedule).permit(:appliance_id, :day, :time_on, :time_off)
+    end
+
+    def update_params
+        params.require(:schedule).permit(:id, :time_on, :time_off, :left_right)
     end
 
 end
